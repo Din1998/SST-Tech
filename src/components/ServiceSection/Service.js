3fee7dbf90e9c8121b6aeefb7053a83880d2d38a
Din1998 <dinislam8803@gmail.com>
2021-01-01 19:31:07 +0600

    thrd3 change

--- src/components/ServiceSection/Service.js
+++ src/components/ServiceSection/Service.js
@@ -4,6 +4,13 @@ import './Service.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col} from 'react-bootstrap';
 
+import img1 from '../ImgAssest/test9.png';
+import img2 from '../ImgAssest/test10.png';
+import img3 from '../ImgAssest/test11.png';
+import img5 from '../ImgAssest/test13.png';
+import img6 from '../ImgAssest/test14.png';
+import img7 from '../ImgAssest/test15.png';
+
 const Service = props => {
 	return (
     <div className="service-main-container">
@@ -19,7 +26,48 @@ const Service = props => {
       <Container>
         <Row>
           <Col>
-            <ServiceUi />
+            <ServiceUi 
+              img={img2}
+              title="web development" 
+              discription="The path of a cosmonaut is not an easy, triumphant march to glory. You have to get to know the meaning not just of joy."
+            />
+          </Col>
+          <Col>
+            <ServiceUi 
+              img={img1}
+              title="analystics" 
+              discription="Dinosaurs are extinct today because they lacked opposable thumbs and the brainpower to build a space program."
+            />
+          </Col>
+          <Col>
+            <ServiceUi 
+              img={img7}
+              title="database structure" 
+              discription="The path of a cosmonaut is not an easy, triumphant march to glory. You have to get to know the meaning not just of joy."
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ServiceUi 
+              img={img5}
+              title="advertising"
+              discription="The path of a cosmonaut is not an easy, triumphant march to glory. You have to get to know the meaning not just of joy."
+            />
+          </Col>
+          <Col>
+            <ServiceUi 
+              img={img3}
+              title="mobile responsive" 
+              discription="Dinosaurs are extinct today because they lacked opposable thumbs and the brainpower to build a space program."
+          />
+          </Col>
+          <Col>
+            <ServiceUi 
+              img={img6}
+              title="graphic design" 
+              discription="Dinosaurs are extinct today because they lacked opposable thumbs and the brainpower to build a space program."
+            />
           </Col>
         </Row>
       </Container>
--- src/components/ServiceSection/ServiceUi.js
+++ src/components/ServiceSection/ServiceUi.js
@@ -4,7 +4,16 @@ import './Serviceui.css';
 
 const ServiceUi = props => {
 	return (
-    <div>wait for some thing new...........</div>
+    <div>
+      <div className="service-icon-box1">
+        <img className="service-icon1" 
+          src={props.img} 
+          alt="" 
+        />
+      </div>
+      <h4 className="service-core-icon-heading">{props.title}</h4>
+      <p className="service-feature-prgph">{props.discription}</p>
+    </div>
   )
 }
 
